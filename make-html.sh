b22c946dc6bf39a85612cc44c8cafb40d0352dc9
Bennett Cyphers <bcyphers@mit.edu>
2020-10-07 02:06:05 +0000

    move out/ to www/

--- make-html.sh
+++ make-html.sh
@@ -4,6 +4,6 @@ pandoc -s md/cpra-both.md -t html -o html/cpra-both.html --metadata pagetitle='C
 pandoc -s md/cpra-new.md -t html -o html/cpra-new.html --metadata pagetitle='CPRA'
 pandoc -s md/cpra-old.md -t html -o html/cpra-old.html --metadata pagetitle='CPRA'
 
-#python process-html.py html/cpra-both.html out/both.html
-python process-html.py html/cpra-new.html out/new.html
-#python process-html.py html/cpra-old.html out/old.html
+#python process-html.py html/cpra-both.html www/both.html
+python process-html.py html/cpra-new.html www/new.html
+python process-html.py html/cpra-old.html www/old.html
